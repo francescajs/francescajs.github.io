c91f859303782d4b38a115dab2db6c467f2ebc0b
Francesca Song <francescajsong@gmail.com>
2019-02-16 00:34:09 -0800

    grid format

--- src/Experiences.js
+++ src/Experiences.js
@@ -14,10 +14,14 @@ class Experiences extends Component {
                     <h1>Experiences</h1>
                 </div>
                 <div className="box">
-                    <ExperiencesBox logo={codebase} description="As a mentored developer in CodeBase, worked on a judging portal for CalHacks in a team of 6! Learned front-end web development as well as backend database management."/>
-                    <ExperiencesBox logo={tapin} description="Interned for Tapin2, a startup that provides a mobile ordering platform for concessions, merchandise at various venues. Contributed to QA and wireframing for a satellite project."/>
-                    <ExperiencesBox logo={gwc} description="After participating in GWC’s Summer Immersion Program, went back as a Teaching Assistant for the program during Summer 2018 at the Walt Disney Company!"/>
-                    <ExperiencesBox logo={auth} description="Interned for Authentic, conducting market research on existing podcast series and launched a pilot podcast “Disrupted”. Took part in the recording and post-production editing, while also participating in recording episodes for an existing program."/>
+                    <div className="boxone">
+                        <ExperiencesBox logo={codebase} description="As a mentored developer in CodeBase, worked on a judging portal for CalHacks in a team of 6! Learned front-end web development as well as backend database management."/>
+                        <ExperiencesBox logo={tapin} description="Interned for Tapin2, a startup that provides a mobile ordering platform for concessions, merchandise at various venues. Contributed to QA and wireframing for a satellite project."/>
+                    </div>
+                    <div className="boxtwo">
+                        <ExperiencesBox logo={gwc} description="After participating in GWC’s Summer Immersion Program, went back as a Teaching Assistant for the program during Summer 2018 at the Walt Disney Company!"/>
+                        <ExperiencesBox logo={auth} description="Interned for Authentic, conducting market research on existing podcast series and launched a pilot podcast “Disrupted”. Took part in the recording and post-production editing, while also participating in recording episodes for an existing program."/>
+                    </div>
                 </div>
             </div>
         );
