4088662afd83f17179a8fd5e0c0e2c4f8a33c287
Francesca Song <francescajsong@gmail.com>
2019-02-17 12:56:34 -0800

    Updates

---
+++ precache-manifest.1b0f1f9ddcc8ccd0dc17546bdd3fc0a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7da0ffa0fed39d1d2272b95341bf68f4",
+    "url": "/static/media/tapin2.7da0ffa0.webp"
+  },
+  {
+    "revision": "fc14af501e1ae25e2edb",
+    "url": "/static/css/main.ad51fe34.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "4e3ee97c85ec4c053ff1",
+    "url": "/static/js/2.4e3ee97c.chunk.js"
+  },
+  {
+    "revision": "358923beda36e420922e07a7d121c3b3",
+    "url": "/static/media/ResumeP.358923be.pdf"
+  },
+  {
+    "revision": "addb526da3c02c4597e8188e2ae9599d",
+    "url": "/static/media/instagram-icon.addb526d.png"
+  },
+  {
+    "revision": "c20eeb3e4cf6dfdbcde6dcb008fd1a97",
+    "url": "/static/media/codebaselogo.c20eeb3e.png"
+  },
+  {
+    "revision": "fc14af501e1ae25e2edb",
+    "url": "/static/js/main.fc14af50.chunk.js"
+  },
+  {
+    "revision": "a502f29da89dc26a9a7c9dc17888bd0c",
+    "url": "/static/media/GWC-logo.a502f29d.jpg"
+  },
+  {
+    "revision": "fe02a689eb4a249c10c64ee52ad33c42",
+    "url": "/static/media/authentic.fe02a689.png"
+  },
+  {
+    "revision": "f7952f8d1bc6274cff4cb9e4a1e5951d",
+    "url": "/static/media/binarysearch.f7952f8d.png"
+  },
+  {
+    "revision": "8b1328a8c7de906b0b7611dbc76687b4",
+    "url": "/static/media/mindthegap.8b1328a8.png"
+  },
+  {
+    "revision": "529088e0f011aeb691d9b54422b4d2a1",
+    "url": "/static/media/homepfran2.529088e0.jpg"
+  },
+  {
+    "revision": "830a15d30ec622f83117f1e7577d684e",
+    "url": "/index.html"
+  }
+];
